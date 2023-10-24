e2a2191ba129342e763f54defb5ad925f468e7a8
Joshua Yong EnFong Suk Koon <joshua.fongsk@gmail.com>
2023-10-24 18:09:35 -0400

    Yes

--- website_files/main.js
+++ website_files/main.js
@@ -1,28 +1,23 @@
-var mouseX = 0, mouseY = 0;
-    var container = document.querySelector('.container');
-    var containerRect = container.getBoundingClientRect();
-    var containerRadius = containerRect.width / 2;
+jQuery(document).ready(function() {
+    var container = $(".container");
+    var circle = $("#circle");
 
-    window.addEventListener('mousemove', function(e) {
-        var containerCenterX = containerRect.left + containerRadius;
-        var containerCenterY = containerRect.top + containerRadius;
-        mouseX = e.pageX - containerCenterX;
-        mouseY = e.pageY - containerCenterY;
+    container.mousemove(function(e){
+        var containerCenterX = container.offset().left + container.width() / 2;
+        var containerCenterY = container.offset().top + container.height() / 2;
+
+        var mouseX = e.pageX - containerCenterX;
+        var mouseY = e.pageY - containerCenterY;
 
         var distance = Math.sqrt(mouseX * mouseX + mouseY * mouseY);
+        var containerRadius = container.width() / 2 - circle.width() / 2; // Adjust for circle size
 
         if (distance > containerRadius) {
             var angle = Math.atan2(mouseY, mouseX);
             mouseX = Math.cos(angle) * containerRadius;
             mouseY = Math.sin(angle) * containerRadius;
         }
-    });
 
-    var circle = document.getElementById('circle');
-    var xp = 0, yp = 0;
-    var loop = setInterval(function() {
-        xp += (mouseX - xp) / 12;
-        yp += (mouseY - yp) / 12;
-        circle.style.left = xp + containerRadius + 'px';
-        circle.style.top = yp + containerRadius + 'px';
-    }, 30);
+        circle.css({ left: mouseX + 'px', top: mouseY + 'px' });
+    });
+});
