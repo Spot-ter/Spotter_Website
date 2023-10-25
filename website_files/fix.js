c60af55b86e9d1b26d15adb16e66af8df0640d21
Ipy <Ipinoluwaheze@gmail.com>
2023-10-24 22:54:52 -0400

    Added fix files and made edits to main.js, style.css and index.html

---
+++ website_files/fix.js
@@ -0,0 +1,40 @@
+var mouseX = 0, mouseY = 0;
+var container = document.querySelector('.container');
+var containerRect = container.getBoundingClientRect();
+var containerRadius = containerRect.width / 2;
+
+// Extend the boundary factor
+var boundaryFactor = 8; // Adjust this value as needed
+
+window.addEventListener('mousemove', function(e) {
+    var containerCenterX = containerRect.left + containerRadius;
+    var containerCenterY = containerRect.top + containerRadius;
+
+    mouseX = e.pageX - containerCenterX;
+    mouseY = e.pageY - containerCenterY;
+
+    var distance = Math.sqrt(mouseX * mouseX + mouseY * mouseY);
+
+    if (distance > containerRadius) {
+        var angle = Math.atan2(mouseY, mouseX);
+        mouseX = Math.cos(angle) * containerRadius * boundaryFactor;
+        mouseY = Math.sin(angle) * containerRadius * boundaryFactor;
+    }
+});
+
+var circle = document.getElementById('circle');
+var loop = setInterval(function() {
+    var xp = mouseX / 12;
+    var yp = mouseY / 12;
+
+    // Calculate the maximum allowed positions
+    var maxX = containerRadius * boundaryFactor - circle.clientWidth / 2;
+    var maxY = containerRadius * boundaryFactor - circle.clientHeight / 2;
+
+    // Ensure the circle stays within the boundaries
+    xp = Math.min(maxX, Math.max(-maxX, xp));
+    yp = Math.min(maxY, Math.max(-maxY, yp));
+
+    circle.style.left = xp + containerRadius - circle.clientWidth / 2 + 'px';
+    circle.style.top = yp + containerRadius - circle.clientHeight / 2 + 'px';
+}, 30);
--- website_files/main.js
+++ website_files/main.js
@@ -1,19 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.addEventListener('mousemove', (e) => {
-        const mouseX = e.clientX;
-        const mouseY = e.clientY;
-        const camera = document.getElementById("camera");
-        const rekt = camera.getBoundingClientRect();
-        const cameraX = rekt.left + rekt.width / 2;
-        const cameraY = rekt.top + rekt.height / 2;
-
-        const angledegree = angle(mouseX, mouseY, cameraX, cameraY);
-        console.log(angledegree);
-
-        document.getElementById("circle1").style.transform = `rotate ${90 + angledegree} deg`;
-
-    });
-
+    
     function angle(cx, cy, ex, ey) {
         const dy = ey - cy;
         const dx = ex - cx;
@@ -21,4 +7,20 @@ document.addEventListener('DOMContentLoaded', function () {
         const degree = rad * 180 / Math.PI;
         return degree;
     }
+
+    document.addEventListener('mousemove', (e) => {
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
+        const camera = document.getElementById('camera');
+        const rekt = camera.getBoundingClientRect();
+        const cameraX = rekt.left + rekt.width / 2;
+        const cameraY = rekt.top + rekt.height / 2;
+
+        const angledegree = angle(mouseX, mouseY, cameraX, cameraY);
+        console.log(angledegree);
+
+        document.getElementById('Circle2').style.transform = `rotate(${90 + angledegree}deg)`;
+
+
+    });
 });
