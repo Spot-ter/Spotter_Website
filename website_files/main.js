ab51004ecf95d2a6efef2f5dc92a23ae452fe2de
Joshua Yong EnFong Suk Koon <joshua.fongsk@gmail.com>
2023-10-25 03:15:50 -0400

    I just remove the fix version and I put all the fix versions on the main

--- website_files/main.js
+++ website_files/main.js
@@ -1,26 +1,40 @@
-document.addEventListener('DOMContentLoaded', function () {
-    
-    function angle(cx, cy, ex, ey) {
-        const dy = ey - cy;
-        const dx = ex - cx;
-        const rad = Math.atan2(dy, dx);
-        const degree = rad * 180 / Math.PI;
-        return degree;
+var mouseX = 0, mouseY = 0;
+var container = document.querySelector('.container');
+var containerRect = container.getBoundingClientRect();
+var containerRadius = containerRect.width / 2;
+
+
+var boundaryFactor = 8; 
+
+window.addEventListener('mousemove', function(e) {
+    var containerCenterX = containerRect.left + containerRadius;
+    var containerCenterY = containerRect.top + containerRadius;
+
+    mouseX = e.pageX - containerCenterX;
+    mouseY = e.pageY - containerCenterY;
+
+    var distance = Math.sqrt(mouseX * mouseX + mouseY * mouseY);
+
+    if (distance > containerRadius) {
+        var angle = Math.atan2(mouseY, mouseX);
+        mouseX = Math.cos(angle) * containerRadius * boundaryFactor;
+        mouseY = Math.sin(angle) * containerRadius * boundaryFactor;
     }
-
-    document.addEventListener('mousemove', (e) => {
-        const mouseX = e.clientX;
-        const mouseY = e.clientY;
-        const camera = document.getElementById('camera');
-        const rekt = camera.getBoundingClientRect();
-        const cameraX = rekt.left + rekt.width / 2;
-        const cameraY = rekt.top + rekt.height / 2;
-
-        const angledegree = angle(mouseX, mouseY, cameraX, cameraY);
-        console.log(angledegree);
-
-        document.getElementById("circle1").style.transform = `rotate(${90 + angledegree}deg)`;
-
-
-    });
 });
+
+var circle = document.getElementById('circle');
+var loop = setInterval(function() {
+    var xp = mouseX / 12;
+    var yp = mouseY / 12;
+
+    
+    var maxX = containerRadius * boundaryFactor - circle.clientWidth / 2;
+    var maxY = containerRadius * boundaryFactor - circle.clientHeight / 2;
+
+    
+    xp = Math.min(maxX, Math.max(-maxX, xp));
+    yp = Math.min(maxY, Math.max(-maxY, yp));
+
+    circle.style.left = xp + containerRadius - circle.clientWidth / 2 + 'px';
+    circle.style.top = yp + containerRadius - circle.clientHeight / 2 + 'px';
+}, 30);
