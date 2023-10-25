478d5bb1f175dcb65223202e3eb68df8f8d00489
Joshua Yong EnFong Suk Koon <joshua.fongsk@gmail.com>
2023-10-24 21:31:45 -0400

    New code for the camera with circle

---
+++ website_files/main.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener('mousemove', (e) => {
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
+        const camera = document.getElementById("camera");
+        const rekt = camera.getBoundingClientRect();
+        const cameraX = rekt.left + rekt.width / 2;
+        const cameraY = rekt.top + rekt.height / 2;
+
+        const angledegree = angle(mouseX, mouseY, cameraX, cameraY);
+        console.log(angledegree);
+
+        document.getElementById("circle1").style.transform = `rotate ${90 + angledegree} deg`;
+
+    });
+
+    function angle(cx, cy, ex, ey) {
+        const dy = ey - cy;
+        const dx = ex - cx;
+        const rad = Math.atan2(dy, dx);
+        const degree = rad * 180 / Math.PI;
+        return degree;
+    }
+});
